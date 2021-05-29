bfb7834d064a906b2fd8bd8faa1a2464d2b22ac7
Mysterion06 <63981288+Mysterion06@users.noreply.github.com>
2021-05-29 02:02:03 +0200

    Add files via upload

---
+++ SRTPluginProviderRE5/SRTPluginProviderRE5/Structs/GameStructs/GamePlayerHP.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SRTPluginProviderRE5.Structs.GameStructs
+{
+    [StructLayout(LayoutKind.Explicit, Pack = 1)]
+
+    public unsafe struct GamePlayerHP
+    {
+        [FieldOffset(0x1366)] public short Max;
+        [FieldOffset(0x1364)] public short Current;
+
+        public static GamePlayerHP AsStruct(byte[] data)
+        {
+            fixed (byte* pb = &data[0])
+            {
+                return *(GamePlayerHP*)pb;
+            }
+        }
+    }
+}
