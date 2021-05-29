15ecbbd81a535a7998d81a0ab3b69027d7a1a3fc
Mysterion06 <63981288+Mysterion06@users.noreply.github.com>
2021-05-29 02:01:20 +0200

    Add files via upload

---
+++ SRTPluginProviderRE5/SRTPluginProviderRE5/PluginInfo.cs
@@ -0,0 +1,26 @@
+﻿using SRTPluginBase;
+using System;
+
+namespace SRTPluginProviderRE5
+{
+    internal class PluginInfo : IPluginInfo
+    {
+        public string Name => "Game Memory Provider (Resident Evil 5 (2008))";
+
+        public string Description => "A game memory provider plugin for Resident Evil 5 (2008).";
+
+        public string Author => "Mysterion_06_, VideoGameRoulette & Squirrelies";
+
+        public Uri MoreInfoURL => new Uri("https://github.com/Mysterion06/SRTPluginProviderRE5");
+
+        public int VersionMajor => assemblyFileVersion.ProductMajorPart;
+
+        public int VersionMinor => assemblyFileVersion.ProductMinorPart;
+
+        public int VersionBuild => assemblyFileVersion.ProductBuildPart;
+
+        public int VersionRevision => assemblyFileVersion.ProductPrivatePart;
+
+        private System.Diagnostics.FileVersionInfo assemblyFileVersion = System.Diagnostics.FileVersionInfo.GetVersionInfo(System.Reflection.Assembly.GetExecutingAssembly().Location);
+    }
+}
