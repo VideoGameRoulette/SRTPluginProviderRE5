6f1c6ed23ad807bda4c585a08f2497df59b142c5
Mysterion06 <63981288+Mysterion06@users.noreply.github.com>
2021-06-03 17:30:13 +0200

    Update IGameMemoryRE5.cs

--- SRTPluginProviderRE5/SRTPluginProviderRE5/IGameMemoryRE5.cs
+++ SRTPluginProviderRE5/SRTPluginProviderRE5/IGameMemoryRE5.cs
@@ -1,11 +1,26 @@
-﻿using System;
+using System;
 using SRTPluginProviderRE5.Structs;
 
 namespace SRTPluginProviderRE5
 {
     public interface IGameMemoryRE5
     {
+        // Chris HP
         short PlayerCurrentHealth { get; set; }
         short PlayerMaxHealth { get; set; }
+
+        // Sheva HP
+        short PlayerCurrentHealth2 { get; set; }
+        short PlayerMaxHealth2 { get; set; }
+
+        // Money
+        int Money { get; set; }
+
+        // Kills Chris
+        int ChrisKills { get; set; }
+
+        // Kills Sheva
+        int ShevaKills { get; set; }
+
     }
 }
