d45cebefcd5f2d5b451b50baeab0952b95bd4eed
Mysterion06 <63981288+Mysterion06@users.noreply.github.com>
2021-06-03 17:30:01 +0200

    Update GameMemoryRE5.cs

--- SRTPluginProviderRE5/SRTPluginProviderRE5/GameMemoryRE5.cs
+++ SRTPluginProviderRE5/SRTPluginProviderRE5/GameMemoryRE5.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Globalization;
 using System.Runtime.InteropServices;
 using SRTPluginProviderRE5.Structs;
@@ -7,13 +7,32 @@ using SRTPluginProviderRE5.Structs.GameStructs;
 namespace SRTPluginProviderRE5
 {
     public class GameMemoryRE5 : IGameMemoryRE5
-    {
+    {   // Chris HP
         public short PlayerCurrentHealth { get => _playerCurrentHealth; set => _playerCurrentHealth = value; }
         internal short _playerCurrentHealth;
 
         public short PlayerMaxHealth { get => _playerMaxHealth; set => _playerMaxHealth = value; }
         internal short _playerMaxHealth;
+        // Sheva HP
+        public short PlayerCurrentHealth2 { get => _playerCurrentHealth2; set => _playerCurrentHealth2 = value; }
+        internal short _playerCurrentHealth2;
 
+        public short PlayerMaxHealth2 { get => _playerMaxHealth2; set => _playerMaxHealth2 = value; }
+        internal short _playerMaxHealth2;
+
+        // Money
+        public int Money { get => _money; set => _money = value; }
+        internal int _money;
+
+        // Kills Chris
+        public int ChrisKills { get => _chrisKills; set => _chrisKills = value; }
+        internal int _chrisKills;
+
+        // Kills Sheva
+        public int ShevaKills { get => _shevaKills; set => _shevaKills = value; }
+        internal int _shevaKills;
+
+        // Enemy HP
         public EnemyHP[] EnemyHealth { get => _enemyHealth; set => _enemyHealth = value; }
         internal EnemyHP[] _enemyHealth;
     }
