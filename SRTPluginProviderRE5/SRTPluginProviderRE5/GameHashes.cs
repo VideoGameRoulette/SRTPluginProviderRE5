d226cc5350681336feccf11aa8028eabbea4c129
Mysterion06 <63981288+Mysterion06@users.noreply.github.com>
2021-06-12 20:57:29 +0200

    Update GameHashes.cs

--- SRTPluginProviderRE5/SRTPluginProviderRE5/GameHashes.cs
+++ SRTPluginProviderRE5/SRTPluginProviderRE5/GameHashes.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
@@ -11,8 +11,9 @@ namespace SRTPluginProviderRE5
     public static class GameHashes
     {
         private static readonly byte[] re5dx9WW_20200922_1 = new byte[32] { 0xF9, 0xD5, 0x04, 0x6D, 0x3C, 0x19, 0xC2, 0xDD, 0xE7, 0xB5, 0xAB, 0xC5, 0x11, 0x4A, 0x04, 0x2D, 0x6D, 0x36, 0xE7, 0x0E, 0x3F, 0xA2, 0x9D, 0x79, 0xDC, 0x53, 0x36, 0xD6, 0xE0, 0x3A, 0x0C, 0x1F }; // Steam WW RTM
+        private static readonly byte[] RE5DX9WW_20090707_1 = new byte[32] { 0x3D, 0x27, 0x99, 0xD3, 0x34, 0xEE, 0x40, 0xE2, 0xBB, 0x28, 0xCA, 0xF8, 0x7C, 0x35, 0xCE, 0xDF, 0xC3, 0x16, 0x2E, 0x78, 0xEE, 0xCB, 0x69, 0x51, 0x0D, 0x83, 0x77, 0xCB, 0x5E, 0x0A, 0x49, 0xA0 };
 
-        public static GameVersion DetectVersion(string filePath)
+        public static string DetectVersion(string filePath)
         {
             byte[] checksum;
             using (SHA256 hashFunc = SHA256.Create())
@@ -21,12 +22,16 @@ namespace SRTPluginProviderRE5
 
             if (checksum.SequenceEqual(re5dx9WW_20200922_1))
             {
-                Console.WriteLine("Steam v1.0 Detected.");
-                return GameVersion.RE5DX9WW_20200922_1;
+                Console.WriteLine("1.1.0");
+                return "1.1.0";
+            } else if (checksum.SequenceEqual(RE5DX9WW_20090707_1))
+            {
+                Console.WriteLine("GFWL");
+                return "GFWL";
             }
 
-            Console.WriteLine("Unknown Version. If you have installed any third party mods, please uninstall and try again.");
-            return GameVersion.Unknown;
+            Console.WriteLine("Unknown Version");
+            return "Unknown Version";
         }
     }
 }
