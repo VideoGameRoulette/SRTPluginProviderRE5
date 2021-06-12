09988c0c09d4690222e111eefb5882db775567bf
Mysterion06 <63981288+Mysterion06@users.noreply.github.com>
2021-06-12 20:56:03 +0200

    Add files via upload

---
+++ SRTPluginProviderRE5/SRTPluginProviderRE5/GameMemoryRE5.cs
@@ -0,0 +1,101 @@
+﻿
+using System;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using SRTPluginProviderRE5.Structs;
+using SRTPluginProviderRE5.Structs.GameStructs;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace SRTPluginProviderRE5
+{
+    public class GameMemoryRE5 : IGameMemoryRE5
+    {   // Chris HP
+        public short PlayerCurrentHealth { get => _playerCurrentHealth; set => _playerCurrentHealth = value; }
+        internal short _playerCurrentHealth;
+
+        public short PlayerMaxHealth { get => _playerMaxHealth; set => _playerMaxHealth = value; }
+        internal short _playerMaxHealth;
+        // Sheva HP
+        public short PlayerCurrentHealth2 { get => _playerCurrentHealth2; set => _playerCurrentHealth2 = value; }
+        internal short _playerCurrentHealth2;
+
+        public short PlayerMaxHealth2 { get => _playerMaxHealth2; set => _playerMaxHealth2 = value; }
+        internal short _playerMaxHealth2;
+
+        // Money
+        public int Money { get => _money; set => _money = value; }
+        internal int _money;
+
+        // Kills Chris
+        public int ChrisKills { get => _chrisKills; set => _chrisKills = value; }
+        internal int _chrisKills;
+
+        // Kills Sheva
+        public int ShevaKills { get => _shevaKills; set => _shevaKills = value; }
+        internal int _shevaKills;
+
+        // Chris DA
+        public short ChrisDA { get => _chrisDA; set => _chrisDA = value; }
+        internal short _chrisDA;
+
+        // Chris DA
+        public short ChrisDARank { get => _chrisDARank; set => _chrisDARank = value; }
+        internal short _chrisDARank;
+
+        // Chris DA
+        public short ShevaDA { get => _shevaDA; set => _shevaDA = value; }
+        internal short _shevaDA;
+
+        // Chris DA
+        public short ShevaDARank { get => _shevaDARank; set => _shevaDARank = value; }
+        internal short _shevaDARank;
+
+        // Chapter
+        public int Chapter { get => _chapter; set => _chapter = value; }
+        internal int _chapter;
+
+        // Enemy HP
+        public EnemyHP[] EnemyHealth { get => _enemyHealth; set => _enemyHealth = value; }
+        internal EnemyHP[] _enemyHealth;
+
+        // Shots Fired
+        public int ShotsFired { get => _shotsfired; set => _shotsfired = value; }
+        internal int _shotsfired;
+
+        // Enemies Hit
+        public int EnemiesHit { get => _enemiesHit; set => _enemiesHit = value; }
+        internal int _enemiesHit;
+
+        // Deaths
+        public int Deaths { get => _deaths; set => _deaths = value; }
+        internal int _deaths;
+
+        // IGT
+        public float IGT { get => _igt; set => _igt = value; }
+        internal float _igt;
+
+        // Shots Fired 2
+        public int ShotsFired2 { get => _shotsfired2; set => _shotsfired2 = value; }
+        internal int _shotsfired2;
+
+        // Enemies Hit 2
+        public int EnemiesHit2 { get => _enemiesHit2; set => _enemiesHit2 = value; }
+        internal int _enemiesHit2;
+
+        // Deaths 2
+        public int Deaths2 { get => _deaths2; set => _deaths2 = value; }
+        internal int _deaths2;
+
+        // IGT 2
+        public float IGT2 { get => _igt2; set => _igt2 = value; }
+        internal float _igt2;
+
+        // Versioninfo
+        public string VersionInfo => FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion;
+
+        // GameInfo
+        public string GameInfo { get =>_gameInfo; set => _gameInfo = value; }
+        internal string _gameInfo;
+    }
+}
---
+++ SRTPluginProviderRE5/SRTPluginProviderRE5/IGameMemoryRE5.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using SRTPluginProviderRE5.Structs;
+using SRTPluginProviderRE5.Structs.GameStructs;
+
+namespace SRTPluginProviderRE5
+{
+    public interface IGameMemoryRE5
+    {
+        // Chris HP
+        short PlayerCurrentHealth { get; set; }
+        short PlayerMaxHealth { get; set; }
+
+        // Sheva HP
+        short PlayerCurrentHealth2 { get; set; }
+        short PlayerMaxHealth2 { get; set; }
+
+        // Money
+        int Money { get; set; }
+
+        // Kills Chris
+        int ChrisKills { get; set; }
+
+        // Kills Sheva
+        int ShevaKills { get; set; }
+
+        // Chris DA
+        short ChrisDA { get; set; }
+
+        // Chris DA Rank
+        short ChrisDARank { get; set; }
+
+        // Sheva DA
+        short ShevaDA { get; set; }
+
+        // Sheva DA Rank
+        short ShevaDARank { get; set; }
+
+        // Chapter
+        public int Chapter { get; set; }
+
+        // Shots Fired
+        int ShotsFired { get; set; }
+
+        // Enemies Hit
+        int EnemiesHit { get; set; }
+
+        // Deaths
+        int Deaths { get; set; }
+
+        // IGT
+        float IGT { get; set; }
+
+        // Shots Fired 2
+        int ShotsFired2 { get; set; }
+
+        // Enemies Hit 2
+        int EnemiesHit2 { get; set; }
+
+        // Deaths 2
+        int Deaths2 { get; set; }
+
+        // IGT 2
+        float IGT2 { get; set; }
+
+        // Versioninfo
+        string VersionInfo { get; }
+
+        // GameInfo
+        string GameInfo { get; set; }
+    }
+}
